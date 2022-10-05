5d878fb0d44be2ede289b4ae6c40f7b0d992938b
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-10-05 23:01:07 +0200

    adding new dtos

---
+++ MagicVillaAPI/Models/Dto/VillaCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVillaAPI.Models.Dto;
+
+public class VillaCreateDto
+{
+    [Required]
+    [MaxLength(30)]
+    public string Name { get; set; } = null!;
+    public int Occupancy { get; set; }
+    public int Surface { get; set; }
+    public string Details { get; set; } = null!;
+    public double Rate { get; set; }
+    public string ImageUrl { get; set; } = null!;
+    public string Amenity { get; set; } = null!;
+}
---
+++ MagicVillaAPI/Models/Dto/VillaUpdateDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVillaAPI.Models.Dto;
+
+public class VillaUpdateDto
+{
+    [Required]
+    public int Id { get; set; }
+    [Required] 
+    [MaxLength(30)] 
+    public string Name { get; set; } = null!;
+    public int Occupancy { get; set; }
+    [Required]
+    public int Surface { get; set; }
+    [Required]
+    public string Details { get; set; } = null!;
+    [Required]
+    public double Rate { get; set; }
+    [Required]
+    public string ImageUrl { get; set; } = null!;
+    public string Amenity { get; set; } = null!;
+}
