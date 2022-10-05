e493583bae7f05f21c7d0de524010ef953e57780
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-10-05 23:01:25 +0200

    implementing repository pattern

---
+++ MagicVillaAPI/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using MagicVillaAPI.Models;
+using MagicVillaAPI.Models.Dto;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaAPI.Repository;
+
+public interface IRepository
+{
+    Task<List<Villa>> GetAll(Expression<Func<Villa>>? filter = null);
+    Task<Villa> Get(Expression<Func<Villa>>? filter = null, bool isTracked = true);
+    Task Create(Villa entity);
+    Task Remove(Villa entity);
+    Task Save();
+}
---
+++ MagicVillaAPI/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using MagicVillaAPI.Data;
+using MagicVillaAPI.Models;
+using MagicVillaAPI.Models.Dto;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVillaAPI.Repository;
+
+class Repository : IRepository
+{
+    private readonly DataContext _dbContext;
+
+
+    public Repository(DataContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<List<Villa>> GetAll(Expression<Func<Villa>>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Villa> Get(Expression<Func<Villa>>? filter = null, bool isTracked = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Create(Villa entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Remove(Villa entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Save()
+    {
+        throw new NotImplementedException();
+    }
+}
