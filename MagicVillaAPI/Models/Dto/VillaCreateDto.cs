c1adf6f699083f72fee36ee29c90813dbc51901a
Lucien <me@fanto.dev>
2024-03-22 22:45:19 +0100

    clean up dtos

--- MagicVillaAPI/Models/Dto/VillaCreateDto.cs
+++ MagicVillaAPI/Models/Dto/VillaCreateDto.cs
@@ -4,9 +4,8 @@ namespace MagicVillaAPI.Models.Dto;
 
 public class VillaCreateDto
 {
-    [Required]
-    [MaxLength(30)]
-    public string Name { get; set; } = null!;
+    [Required] [MaxLength(30)] public string Name { get; set; } = null!;
+
     public int Occupancy { get; set; }
     public int Surface { get; set; }
     public string Details { get; set; } = null!;
--- MagicVillaAPI/Models/Dto/VillaUpdateDto.cs
+++ MagicVillaAPI/Models/Dto/VillaUpdateDto.cs
@@ -4,19 +4,19 @@ namespace MagicVillaAPI.Models.Dto;
 
 public class VillaUpdateDto
 {
-    [Required]
-    public int Id { get; set; }
-    [Required] 
-    [MaxLength(30)] 
-    public string Name { get; set; } = null!;
+    [Required] public int Id { get; set; }
+
+    [Required] [MaxLength(30)] public string Name { get; set; } = null!;
+
     public int Occupancy { get; set; }
-    [Required]
-    public int Surface { get; set; }
-    [Required]
-    public string Details { get; set; } = null!;
-    [Required]
-    public double Rate { get; set; }
-    [Required]
-    public string ImageUrl { get; set; } = null!;
+
+    [Required] public int Surface { get; set; }
+
+    [Required] public string Details { get; set; } = null!;
+
+    [Required] public double Rate { get; set; }
+
+    [Required] public string ImageUrl { get; set; } = null!;
+
     public string Amenity { get; set; } = null!;
 }
